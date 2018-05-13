92073ce235ec080d19a860e1b46393eb65ab734c
Jared Peterson <jpeterson5@stu.parkland.edu>
2018-05-13 00:43:04 -0500

    progress

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function revealMessage(){
-    document.getElementById("hiddenMessage").style.display='block';
+$(document).ready(function(){
+    faqControl();
+});
+
+function faqControl(){
+    $(".a").hide();
+    $(".q").on("click".function(){
+        $(this).next().slideToggle();
+    });
 }
