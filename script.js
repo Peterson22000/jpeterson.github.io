7d56bae41aca1bd528a2e7a624c28f71ba03b283
Jared Peterson <jpeterson5@stu.parkland.edu>
2018-05-14 13:16:51 -0500

    change

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
     var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'http://github.com/Peterson22000/peterson22000.github.io/blob/master/fortunes.json');
+ourRequest.open("GET", "http://github.com/Peterson22000/peterson22000.github.io/blob/master/fortunes.json");
 ourRequest.onload = function(){
   var ourData = json.parse(ourRequest.responseText);
-  console.log(ourData[0]);
 };
 ourRequest.send();
 });
