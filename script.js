5b1662f749cd9051d5e141ec89a5696978b48367
Jared Peterson <jpeterson5@stu.parkland.edu>
2018-05-14 23:43:52 -0500

    finalsubmit

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var btn = document.getElementById("btn");
+var container = document.getElementById("your-fortune");
+var button = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
-    var ourRequest = new XMLHttpRequest();
+var ourRequest = new XMLHttpRequest();
 ourRequest.open("GET", "http://github.com/Peterson22000/peterson22000.github.io/blob/master/fortunes.json");
 ourRequest.onload = function(){
-  var ourData = json.parse(ourRequest.responseText);
+    var ourData = json.parse(ourRequest.responseText);
+    renderHTML(ourData);
 };
 ourRequest.send();
 });
 
+function renderHTML(data){
+    var htmlString = "this is a test";
+ container.insertAdjacentHTML("beforeend", htmlString);
+}
 
 
